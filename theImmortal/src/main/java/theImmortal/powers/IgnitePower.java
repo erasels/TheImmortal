7240c5c344d1d6565757c5304aa932a8fa1f8026
erasels <emanuelbedburdick@yahoo.de>
2019-09-21 18:24:40 +0200

    Added fire particles and tint to Ignited enemies

--- theImmortal/src/main/java/theImmortal/powers/IgnitePower.java
+++ theImmortal/src/main/java/theImmortal/powers/IgnitePower.java
@@ -1,7 +1,12 @@
 package theImmortal.powers;
 
 import basemod.interfaces.CloneablePowerInterface;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.megacrit.cardcrawl.actions.AbstractGameAction;
 import com.megacrit.cardcrawl.actions.common.ReducePowerAction;
 import com.megacrit.cardcrawl.actions.common.RemoveSpecificPowerAction;
@@ -10,17 +15,28 @@ import com.megacrit.cardcrawl.core.AbstractCreature;
 import com.megacrit.cardcrawl.core.CardCrawlGame;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
 import com.megacrit.cardcrawl.localization.PowerStrings;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
 import com.megacrit.cardcrawl.powers.AbstractPower;
 import com.megacrit.cardcrawl.rooms.AbstractRoom;
 import theImmortal.TheImmortal;
 import theImmortal.powers.abstracts.AbstractImmortalPower;
 import theImmortal.util.UC;
+import theImmortal.vfx.general.StraightFireParticle;
+
+import java.util.Random;
 
 public class IgnitePower extends AbstractImmortalPower implements CloneablePowerInterface {
     public static final String POWER_ID = TheImmortal.makeID("Ignite");
     private static final PowerStrings powerStrings = CardCrawlGame.languagePack.getPowerStrings(POWER_ID);
     public static final String NAME = powerStrings.NAME;
     public static final String[] DESCRIPTIONS = powerStrings.DESCRIPTIONS;
+
+    private Random rng = new Random();
+    public static Color tintCol = Color.ORANGE.cpy().add(0.1f, 0.3f, 0.3f, 1.0f);
+
+    private static final float INTERVAL = 0.05f;
+
+    private float particleTimer;
 
     public IgnitePower(AbstractCreature owner, int amount) {
         name = NAME;
@@ -42,7 +58,7 @@ public class IgnitePower extends AbstractImmortalPower implements CloneablePower
     public void atEndOfRound() {
         if (AbstractDungeon.getCurrRoom().phase == AbstractRoom.RoomPhase.COMBAT) {
             if (!AbstractDungeon.getMonsters().areMonstersBasicallyDead()) {
-                int stack = MathUtils.ceil(amount/2.0f);
+                int stack = MathUtils.ceil(amount / 2.0f);
                 flashWithoutSound();
                 UC.doDmg(owner, stack, DamageInfo.DamageType.THORNS, AbstractGameAction.AttackEffect.FIRE);
 
@@ -51,6 +67,29 @@ public class IgnitePower extends AbstractImmortalPower implements CloneablePower
                 } else {
                     UC.atb(new ReducePowerAction(this.owner, this.owner, this, stack));
                 }
+            }
+        }
+    }
+
+    @Override
+    public void updateParticles() {
+        this.particleTimer -= Gdx.graphics.getDeltaTime();
+        if (this.particleTimer < 0.0F) {
+            float xOff = ((owner.hb_w) * (float) rng.nextGaussian())*0.25f;
+            if(MathUtils.randomBoolean()) {
+                xOff = -xOff;
+            }
+            AbstractDungeon.effectList.add(new StraightFireParticle(owner.drawX + xOff, owner.drawY + MathUtils.random(owner.hb_h/2f), 75f));
+            this.particleTimer = INTERVAL;
+        }
+    }
+
+    @SpirePatch(clz = AbstractMonster.class, method = "render")
+    public static class ChangeColorOfAffectedMonsterPls {
+        @SpirePrefixPatch
+        public static void patch(AbstractMonster __instance, SpriteBatch sb) {
+            if(__instance.hasPower(IgnitePower.POWER_ID)) {
+                __instance.tint.color.mul(IgnitePower.tintCol);
             }
         }
     }
