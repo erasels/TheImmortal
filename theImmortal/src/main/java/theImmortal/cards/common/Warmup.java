faf3d1aaabdd213019b9600cccb77736b1ffde9f
erasels <emanuelbedburdick@yahoo.de>
2019-09-28 19:21:37 +0200

    Added card Warmup

---
+++ theImmortal/src/main/java/theImmortal/cards/common/Warmup.java
@@ -0,0 +1,39 @@
+package theImmortal.cards.common;
+
+import com.megacrit.cardcrawl.actions.common.DrawCardAction;
+import com.megacrit.cardcrawl.characters.AbstractPlayer;
+import com.megacrit.cardcrawl.monsters.AbstractMonster;
+import com.megacrit.cardcrawl.vfx.combat.OfferingEffect;
+import theImmortal.cards.abstracts.ImmortalCard;
+import theImmortal.util.CardInfo;
+import theImmortal.util.UC;
+
+import static theImmortal.TheImmortal.makeID;
+
+public class Warmup extends ImmortalCard {
+    private final static CardInfo cardInfo = new CardInfo(
+            "Warmup",
+            0,
+            CardType.SKILL,
+            CardTarget.SELF
+    );
+
+    public final static String ID = makeID(cardInfo.cardName);
+
+    private static final int MAGIC = 2;
+    private static final int UPG_MAGIC = 1;
+    private static final int HPLOSS = 3;
+
+    public Warmup() {
+        super(cardInfo, false);
+
+        setHPCost(HPLOSS);
+        setMagic(MAGIC, UPG_MAGIC);
+    }
+
+    @Override
+    public void use(AbstractPlayer p, AbstractMonster m) {
+        UC.doVfx(new OfferingEffect());
+        UC.atb(new DrawCardAction(magicNumber));
+    }
+}
