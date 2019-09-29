59e37693500e089f767173bfd7353ad212a8a42c
erasels <emanuelbedburdick@yahoo.de>
2019-09-29 14:25:06 +0200

    Update ImmortalFlamePatches.java

--- theImmortal/src/main/java/theImmortal/patches/cards/ImmortalFlamePatches.java
+++ theImmortal/src/main/java/theImmortal/patches/cards/ImmortalFlamePatches.java
@@ -2,6 +2,7 @@ package theImmortal.patches.cards;
 
 import com.evacipated.cardcrawl.mod.stslib.actions.common.FetchAction;
 import com.evacipated.cardcrawl.modthespire.lib.SpirePatch;
+import com.evacipated.cardcrawl.modthespire.lib.SpirePrefixPatch;
 import com.megacrit.cardcrawl.cards.AbstractCard;
 import com.megacrit.cardcrawl.cards.CardGroup;
 import com.megacrit.cardcrawl.dungeons.AbstractDungeon;
@@ -14,33 +15,29 @@ public class ImmortalFlamePatches {
     @SpirePatch(clz = CardGroup.class, method = "moveToBottomOfDeck")
     @SpirePatch(clz = CardGroup.class, method = "resetCardBeforeMoving")
     public static class StaysInHand {
-        public static void Prefix(CardGroup __instance, AbstractCard card) {
+        @SpirePrefixPatch
+        public static void patch(CardGroup __instance, AbstractCard card) {
             if (card.cardID.equals(ImmortalFlame.ID)) {
-                AbstractDungeon.actionManager.addToTop(new FetchAction(UC.p().exhaustPile, c -> (c == card), c-> {
-                    if(c.get(0) != null) ((ImmortalFlame) c.get(0)).trigger();
-                    UC.p().hand.glowCheck();
-                }));
-                AbstractDungeon.actionManager.addToTop(new FetchAction(UC.p().drawPile, c -> (c == card), c-> {
-                    if(c.get(0) != null) ((ImmortalFlame) c.get(0)).trigger();
-                    UC.p().hand.glowCheck();
-                }));
-                AbstractDungeon.actionManager.addToTop(new FetchAction(UC.p().discardPile, c -> (c == card), c-> {
-                    if(c.get(0) != null) ((ImmortalFlame) c.get(0)).trigger();
-                    UC.p().hand.glowCheck();
-                }));
+                AbstractDungeon.actionManager.addToTop(new FetchAction(UC.p().exhaustPile, c -> (c.uuid == card.uuid), c-> triggerFlame(c.get(0))));
+                AbstractDungeon.actionManager.addToTop(new FetchAction(UC.p().drawPile, c -> (c.uuid == card.uuid), c-> triggerFlame(c.get(0))));
+                AbstractDungeon.actionManager.addToTop(new FetchAction(UC.p().discardPile, c -> (c.uuid == card.uuid), c-> triggerFlame(c.get(0))));
             }
         }
     }
 
     @SpirePatch(clz = CardGroup.class, method = "moveToDeck")
     public static class StaysInHand2 {
-        public static void Prefix(CardGroup __instance, AbstractCard card, boolean randomSpot) {
+        @SpirePrefixPatch
+        public static void patch(CardGroup __instance, AbstractCard card, boolean randomSpot) {
             if (card.cardID.equals(ImmortalFlame.ID) && __instance == AbstractDungeon.player.hand && AbstractDungeon.player.hand.contains(card)) {
-                AbstractDungeon.actionManager.addToTop(new FetchAction(UC.p().exhaustPile, c -> (c == card), c-> {
-                    if(c.get(0) != null) ((ImmortalFlame) c.get(0)).trigger();
-                    UC.p().hand.glowCheck();
-                }));
+                AbstractDungeon.actionManager.addToTop(new FetchAction(UC.p().exhaustPile, c -> (c.uuid == card.uuid), c-> triggerFlame(c.get(0))));
             }
         }
+    }
+
+    private static void triggerFlame(AbstractCard c) {
+        if(c == null) return;
+        ((ImmortalFlame)c).trigger();
+        UC.p().hand.glowCheck();
     }
 }
