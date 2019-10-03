6d7f919dea710d931178fe92187daca262d75bba
erasels <emanuelbedburdick@yahoo.de>
2019-10-03 14:14:10 +0200

    Buffed Warmup

--- theImmortal/src/main/java/theImmortal/cards/common/Warmup.java
+++ theImmortal/src/main/java/theImmortal/cards/common/Warmup.java
@@ -20,7 +20,7 @@ public class Warmup extends ImmortalCard {
 
     public final static String ID = makeID(cardInfo.cardName);
 
-    private static final int MAGIC = 2;
+    private static final int MAGIC = 3;
     private static final int UPG_MAGIC = 1;
     private static final int HPLOSS = 3;
 
